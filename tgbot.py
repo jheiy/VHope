5df8f9c9ca7ae8f201b7ef00d9ed195cc91e89b8
jaimepastor <jjaimepastor@gmail.com>
2021-07-13 12:04:00 +0800

    start conversation function

--- tgbot.py
+++ tgbot.py
@@ -11,16 +11,32 @@ import telebot
 TOKEN = '1772588897:AAEykF5UTlq7qWQXuFqpGfjPSrFkliQcaes'
 
 bot = telebot.TeleBot(TOKEN)
+orsen = ORSEN()
+Logger.setup_loggers()
+is_engaged = False
 
 @bot.message_handler(commands=['start'])
 def opening_message(message):
     bot.send_message(message.chat.id, "Hey")
-    pass
-
-@bot.message_handler(commands=['chat'])
-def start_conversation(message):
-    # bot.reply_to(message.message_id, message.text)
     
-    pass
+
+@bot.message_handler(commands=['start_chatting'])
+def start_conversation(message):
+    is_engaged = True
+    # bot.reply_to(message.message_id, message.text)
+    bot.send_message(message.chat.id, "Okay, let's begin!")
+    orsen.initialize_story_prerequisites()
+    orsen.world.reset_world()
+    orsen.dialogue_planner.reset_new_world()
+
+    temp_welcome = orsen.get_response(move_to_execute = orsen.dialogue_planner.get_welcome_message_type())
+    print(temp_welcome)
+
+@bot.message_handler(func=lambda m:True)
+def continue_conversation(message):
+    if is_engaged == True:
+        pass    
+    
+    
 
 bot.polling()
