2840c9874a0568edf57a0419b916ef39d2eaa134
jheiy <jackylyn_beredo@dlsu.edu.ph>
2022-02-06 19:38:52 +0800

    Create delete_logs.py
    script for deleting all past logs in all folders for easier cleanup

---
+++ delete_logs.py
@@ -0,0 +1,15 @@
+import os, shutil
+
+folder = ['logs/conversation', 'logs/dialogue model', 'logs/emotion classification', 'logs/event chain', 'logs/information extraction', 'logs/user world', 'logs/vhope']
+
+for names in folder:
+    for filename in os.listdir(names):
+        file_path = os.path.join(names, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+            print('%s successfully deleted.' %filename)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
